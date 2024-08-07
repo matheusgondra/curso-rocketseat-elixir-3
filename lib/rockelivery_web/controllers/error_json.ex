92ba64eb06d25158bea395319ea407d5d8460467
matheusgondra <matheusgondra951@gmail.com>
2024-08-06 21:40:46 -0300

    tratando erros com fallback controller

--- lib/rockelivery_web/controllers/error_json.ex
+++ lib/rockelivery_web/controllers/error_json.ex
@@ -5,17 +5,23 @@ defmodule RockeliveryWeb.ErrorJSON do
   See config/config.exs.
   """
 
-  # If you want to customize a particular status code,
-  # you may add your own clauses, such as:
-  #
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
+  alias Ecto.Changeset
+
+  def render("400.json", %{result: %Changeset{} = changeset}) do
+    %{message: translate_errors(changeset)}
+  end
+
   def render(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
+    end)
   end
 end
--- lib/rockelivery_web/controllers/user_controller.ex
+++ lib/rockelivery_web/controllers/user_controller.ex
@@ -1,7 +1,10 @@
 defmodule RockeliveryWeb.UserController do
   use RockeliveryWeb, :controller
 
+  alias RockeliveryWeb.FallbackController
   alias Rockelivery.Users.User
+
+  action_fallback FallbackController
 
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
