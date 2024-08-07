f4d5ef119ca376e853f74777ed2145a4d2e1215a
Matheus de Gondra <matheusgondra951@gmail.com>
2024-08-07 07:43:03 -0300

    testando criação do usuário

---
+++ test/rockelivery/users/create_test.exs
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Users.CreateTest do
+  alias Rockelivery.Error
+  use Rockelivery.DataCase, async: true
+
+  import Rockelivery.Factory
+
+  alias Rockelivery.Users.Create
+  alias Rockelivery.Users.User
+
+  describe "call/1" do
+    test "when all params are valid, returns the user" do
+      params = build(:user_params)
+
+      response = Create.call(params)
+
+      assert {:ok, %User{id: _id, age: 18, email: "fulano@mail.com"}} = response
+    end
+
+    test "when there are invalid params, returns the error" do
+      params = build(:user_params, password: "123", age: 15)
+
+      response = Create.call(params)
+
+      expected_response = %{
+        password: ["should be at least 6 character(s)"],
+        age: ["must be greater than or equal to 18"]
+      }
+
+      assert {:error, %Error{result: changeset, status: :bad_request}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
